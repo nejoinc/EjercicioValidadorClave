ee07b7f972f0c6bb1eaceda1bef47efc44f6829a
NejoInc <josemanuelvalenciavelez@gmail.com>
2025-10-22 15:19:42 -0500

    Implement Validador

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -102,3 +102,9 @@ class ReglaValidacionCalisto(ReglaValidacion):
 
         return True
 
+class Validador:
+    def __init__(self, regla: ReglaValidacion):
+        self.regla = regla
+
+    def es_valida(self, clave: str) -> bool:
+        return self.regla.es_valida(clave)
