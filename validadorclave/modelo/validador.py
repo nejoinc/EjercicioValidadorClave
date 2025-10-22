fe003fa86a6a24b47e6346babecdc3313b21d538
NejoInc <josemanuelvalenciavelez@gmail.com>
2025-10-22 15:19:05 -0500

    Implement ReglaValidacionGanimedes

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,4 +1,12 @@
 from abc import ABC, abstractmethod
+from validadorclave.modelo.errores import (
+    NoCumpleLongitudMinimaError,
+    NoTieneLetraMayusculaError,
+    NoTieneLetraMinusculaError,
+    NoTieneNumeroError,
+    NoTieneCaracterEspecialError,
+    NoTienePalabraSecretaError
+)
 
 class ReglaValidacion(ABC):
     def __init__(self, longitud_esperada: int):
@@ -28,3 +36,33 @@ class ReglaValidacion(ABC):
     @abstractmethod
     def es_valida(self, clave: str) -> bool:
         pass
+
+
+class ReglaValidacionGanimedes(ReglaValidacion):
+    def __init__(self):
+        super().__init__(longitud_esperada=8)
+
+    def contiene_caracter_especial(self, clave: str) -> bool:
+        caracteres_especiales = ['@', '_', '#', '$', '%']
+        for caracter in clave:
+            if caracter in caracteres_especiales:
+                return True
+        return False
+
+    def es_valida(self, clave: str) -> bool:
+        if not self._validar_longitud(clave):
+            raise NoCumpleLongitudMinimaError("La clave debe tener una longitud de más de 8 caracteres")
+
+        if not self._contiene_mayuscula(clave):
+            raise NoTieneLetraMayusculaError("La clave debe contener al menos una letra mayúscula")
+
+        if not self._contiene_minuscula(clave):
+            raise NoTieneLetraMinusculaError("La clave debe contener al menos una letra minúscula")
+
+        if not self._contiene_numero(clave):
+            raise NoTieneNumeroError("La clave debe contener al menos un número")
+
+        if not self.contiene_caracter_especial(clave):
+            raise NoTieneCaracterEspecialError("La clave debe contener al menos un caracter especial (@, _, #, $, %)")
+
+        return True
